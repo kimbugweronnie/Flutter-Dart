fd951c7e4a8835a4727b3e36a9a4e9dd25d2aa92
Ronnie Kimbugwe <31738584+kimbugweronnie@users.noreply.github.com>
2023-07-23 20:22:17 +0300

    Update 7c.highest-lowest.dart

--- 7c.highest-lowest.dart
+++ 7c.highest-lowest.dart
@@ -11,7 +11,7 @@ void main() {
   Iterable<String> keys = jsonObject.keys;
   for (int i = 0; i < keys.length; i++) {
      Type variableType = jsonObject[keys.elementAt(i)].runtimeType;
-     months[keys.elementAt(i)] = variableType == int ? jsonObject[keys.elementAt(i)] : methodB(jsonObject[keys.elementAt(i)]);     
+     months[keys.elementAt(i)] = variableType == int ? jsonObject[keys.elementAt(i)] : getTotal(jsonObject[keys.elementAt(i)]);     
   }
   int highest = months.values.reduce((maxValue, value) => maxValue < value ? maxValue : value);
   int lowest = months.values.reduce((maxValue, value) => maxValue > value ? maxValue : value);
@@ -27,7 +27,7 @@ void main() {
   print(highestlowestmonths);
 }
 
-int methodB(List<int> numbers ) {
+int getTotal(List<int> numbers ) {
   int total = 0;
   for (int i = 0; i < numbers.length; i++) {
        total = total + numbers[i];  
