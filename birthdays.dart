b3972a1c536338b42c019d1a29c0e660e52b1745
Ronnie Kimbugwe <31738584+kimbugweronnie@users.noreply.github.com>
2023-07-23 15:42:50 +0300

    Update birthdays.dart

---
+++ birthdays.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+void main() {
+ Map<String, dynamic> months = {};
+ Map<String, dynamic> jsonObject = {
+    'May':[3,5],
+    'November':2,
+    'December': [1,4,2] 
+  };
+  String jsonString = jsonEncode(jsonObject);
+  Map<String, dynamic> jsonObject1 = jsonDecode(jsonString);
+  Iterable<String> keys = jsonObject1.keys;
+  for (int i = 0; i < keys.length; i++) {
+     Type variableType = jsonObject[keys.elementAt(i)].runtimeType;
+     months[keys.elementAt(i)] = variableType == int ? jsonObject[keys.elementAt(i)] : methodB(jsonObject[keys.elementAt(i)]);
+  }
+  print(months);
+}
+int methodB(List<int> numbers ) {
+  int total = 0;
+  for (int i = 0; i < numbers.length; i++) {
+       total = total + numbers[i];  
+  }
+  return total;
+}
