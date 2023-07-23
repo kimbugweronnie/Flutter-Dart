f8c8fbf165b61a36150390ee6b663406e1bacac3
Ronnie Kimbugwe <31738584+kimbugweronnie@users.noreply.github.com>
2023-07-23 20:22:48 +0300

    Update 7b.birthdays.dart

--- 7b.birthdays.dart
+++ 7b.birthdays.dart
@@ -8,11 +8,11 @@ void main() {
   Iterable<String> keys = jsonObject.keys;
   for (int i = 0; i < keys.length; i++) {
      Type variableType = jsonObject[keys.elementAt(i)].runtimeType;
-     months[keys.elementAt(i)] = variableType == int ? jsonObject[keys.elementAt(i)] : methodB(jsonObject[keys.elementAt(i)]);
+     months[keys.elementAt(i)] = variableType == int ? jsonObject[keys.elementAt(i)] : getTotal(jsonObject[keys.elementAt(i)]);
   }
   print(months);
 }
-int methodB(List<int> numbers ) {
+int getTotal(List<int> numbers ) {
   int total = 0;
   for (int i = 0; i < numbers.length; i++) {
        total = total + numbers[i];  
