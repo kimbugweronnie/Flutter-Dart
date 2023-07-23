445ecfb2446f544a777500c5ba6c5e7ee7d1a033
Ronnie Kimbugwe <31738584+kimbugweronnie@users.noreply.github.com>
2023-07-23 19:01:05 +0300

    Update 7b.birthdays.dart

--- 7b.birthdays.dart
+++ 7b.birthdays.dart
@@ -5,7 +5,6 @@ void main() {
     'November':2,
     'December': [1,4,2] 
   };
- 
   Iterable<String> keys = jsonObject.keys;
   for (int i = 0; i < keys.length; i++) {
      Type variableType = jsonObject[keys.elementAt(i)].runtimeType;
