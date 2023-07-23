dc32e0c35b961013132018691b35319678bcc2cc
Ronnie Kimbugwe <31738584+kimbugweronnie@users.noreply.github.com>
2023-07-23 16:37:27 +0300

    Update highest.dart

---
+++ highest.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+void main() {
+ Map<String, dynamic> months = {};
+ Map<String, dynamic> highestlowestmonths = {};
+ Map<String, dynamic> jsonObject = {
+    'May':[3,5],
+    'November':2,
+    'December': [1,4,2] 
+  };
+  
+  String jsonString = jsonEncode(jsonObject);
+  Map<String, dynamic> jsonObject1 = jsonDecode(jsonString);
+  Iterable<String> keys = jsonObject1.keys;
+  for (int i = 0; i < keys.length; i++) {
+     Type variableType = jsonObject[keys.elementAt(i)].runtimeType;
+     months[keys.elementAt(i)] = variableType == int ? jsonObject[keys.elementAt(i)] : methodB(jsonObject[keys.elementAt(i)]);     
+  }
+  int highest = months.values.reduce((maxValue, value) => maxValue < value ? maxValue : value);
+  int lowest = months.values.reduce((maxValue, value) => maxValue > value ? maxValue : value);
+  
+  for (int i = 0; i < keys.length; i++) {
+    if(months[keys.elementAt(i)] == highest){
+      highestlowestmonths[keys.elementAt(i)] = months[keys.elementAt(i)];
+      
+    }
+    if(months[keys.elementAt(i)] == lowest){
+       highestlowestmonths[keys.elementAt(i)] = months[keys.elementAt(i)];
+    }  
+  }
+  print(highestlowestmonths);
+}
+
+int methodB(List<int> numbers ) {
+  int total = 0;
+  for (int i = 0; i < numbers.length; i++) {
+       total = total + numbers[i];  
+  }
+  return total;
+}
