2cc2a41d4e29b6d234896eeec303664e79cbcb95
Ronnie Kimbugwe <31738584+kimbugweronnie@users.noreply.github.com>
2023-07-23 19:04:31 +0300

    Update 7c.highest-lowest.dart

--- 7c.highest-lowest.dart
+++ 7c.highest-lowest.dart
@@ -1,4 +1,4 @@
-import 'dart:convert';
+
 void main() {
  Map<String, dynamic> months = {};
  Map<String, dynamic> highestlowestmonths = {};
@@ -8,9 +8,7 @@ void main() {
     'December': [1,4,2] 
   };
   
-  String jsonString = jsonEncode(jsonObject);
-  Map<String, dynamic> jsonObject1 = jsonDecode(jsonString);
-  Iterable<String> keys = jsonObject1.keys;
+  Iterable<String> keys = jsonObject.keys;
   for (int i = 0; i < keys.length; i++) {
      Type variableType = jsonObject[keys.elementAt(i)].runtimeType;
      months[keys.elementAt(i)] = variableType == int ? jsonObject[keys.elementAt(i)] : methodB(jsonObject[keys.elementAt(i)]);     
@@ -20,8 +18,7 @@ void main() {
   
   for (int i = 0; i < keys.length; i++) {
     if(months[keys.elementAt(i)] == highest){
-      highestlowestmonths[keys.elementAt(i)] = months[keys.elementAt(i)];
-      
+      highestlowestmonths[keys.elementAt(i)] = months[keys.elementAt(i)]; 
     }
     if(months[keys.elementAt(i)] == lowest){
        highestlowestmonths[keys.elementAt(i)] = months[keys.elementAt(i)];
